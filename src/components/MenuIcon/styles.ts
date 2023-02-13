d5f602673a9bd6f3a97fc69729e92e5ebcb9e0b5
Jhonatanmizu <natanjesuss20@gmail.com>
2023-02-12 22:39:40 -0300

    :sparkles: created MenuIcon

---
+++ src/components/MenuIcon/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+interface LayoutProps {
+  barTransform?: 'transform' | 'transformInverse' | 'opacity' | 'none'
+}
+
+export const transformations = {
+  transform: 'translate(0, 11px) rotate(45deg)',
+  transformInverse: ' translate(0, -11px) rotate(-45deg)',
+  opacity: '0',
+  none: ''
+}
+
+export const Wrapper = styled.div`
+  display: inline-block;
+  cursor: pointer;
+`
+
+export const Bar = styled.div<LayoutProps>`
+  width: 3.6rem;
+  height: 0.5rem;
+  background-color: ${({ theme }) => theme.palette.common.white};
+  margin: 0.6rem 0;
+  transition: 0.4s;
+  opacity: ${(props) =>
+    props.barTransform === 'opacity'
+      ? transformations[props.barTransform]
+      : '1'};
+  transform: ${(props) =>
+    props.barTransform ? transformations[props.barTransform] : null};
+`
