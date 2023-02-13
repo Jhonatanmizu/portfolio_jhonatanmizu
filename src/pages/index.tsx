f69f02bc8a219113360b47a84f81e320259dead0
Jhonatanmizu <natanjesuss20@gmail.com>
2023-02-12 22:40:52 -0300

    :sparkles: created Menu and Nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Heading } from '@/components/Heading'
+import Menu from '@/components/Menu'
+import MenuIcon from '@/components/MenuIcon'
 import * as S from '@/layout/containers'
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <main>
       <S.Container>
-        <Heading />
+        <Heading title="A Software Developer." />
+        {/* <MenuIcon /> */}
+        <Menu />
       </S.Container>
     </main>
   )
 }
+export default Home
