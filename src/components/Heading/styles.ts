83b601862a1a0eca077d4ece18869e59f5a42e47
Jhonatanmizu <natanjesuss20@gmail.com>
2023-02-19 00:00:13 -0300

    :recycle: optimized styles

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const Heading = styled.h1`
   color: ${({ theme }) => theme.palette.secondary.contrastText};
   font-size: ${({ theme }) => theme.typography.lg};
-  font-family: ${({ theme }) => theme.fonts[1]};
+  font-family: ${({ theme }) => theme.fonts[0]};
   font-weight: 700;
 `
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
 import { Heading } from '@/components/Heading'
-import Menu from '@/components/Menu'
-import MenuIcon from '@/components/MenuIcon'
+import Nav from '@/components/Nav'
 import * as S from '@/layout/containers'
+import { fetchPosts } from '@/services/posts'
+import { GetStaticProps } from 'next'
 
-const Home: React.FC = () => {
+interface HomeProps {
+  posts: object[]
+}
+
+const Home = ({ posts }: HomeProps) => {
   return (
     <main>
-      <S.Container>
+      <Nav />
+      <S.Container id="about">
         <Heading title="A Software Developer." />
-        {/* <MenuIcon /> */}
-        <Menu />
+      </S.Container>
+      <S.Container id="skills">
+        <Heading title="A Software Developer 2." />
+      </S.Container>
+      <S.Container id="projects">
+        <Heading title="A Software Developer 3." />
       </S.Container>
     </main>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await fetchPosts()
+  return {
+    props: {
+      posts
+    }
+  }
 }
 export default Home
